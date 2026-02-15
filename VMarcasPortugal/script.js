04dbdc5dba235fa162ce61f0537e3b40aef61168
CROW77 <mairamillerdonega@gmail.com>
2026-02-15 14:19:00 -0300

    Update script.js

--- VMarcasPortugal/script.js
+++ VMarcasPortugal/script.js
@@ -72,6 +72,7 @@ async function carregarDados() {
         const response = await fetch(url);
         const data = await response.json();
 
+        const membersFromJson = Number(data.members) || 0;
         document.getElementById('members').textContent = membersFromJson;
         document.getElementById('donations').textContent = `$${data.donations ?? 0}`;
         document.getElementById('bosses').textContent = data.bosses ?? 0;
@@ -137,6 +138,7 @@ window.addEventListener("scroll", () => {
     }
     lastScroll = current;
 });
+
 
 
 
